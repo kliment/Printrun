d3155e02a182b74f5d1bc6e2cff675c66a75c5b0
Gary Hodgson <contact@garyhodgson.com>
2011-07-31 16:22:10 +0200

    Improved layout for smaller screens.  Had to introduce a hack to use different sizes when on Windows versus other systems.

--- SkeinforgeQuickEditDialog.py
+++ SkeinforgeQuickEditDialog.py
@@ -8,13 +8,22 @@ import os
 import wx
 
 class SkeinforgeQuickEditDialog(wx.Dialog):
-    def __init__(self, *args, **kwds):        
-        wx.Dialog.__init__(self,*args, **kwds)
+    '''Shows a consise list of important settings from the active Skeinforge profile.'''
+    def __init__(self, *args, **kwds):
+        kwds["style"] = wx.DEFAULT_DIALOG_STYLE | wx.MAXIMIZE_BOX | wx.MINIMIZE_BOX | wx.RESIZE_BORDER
+        wx.Dialog.__init__(self, *args, **kwds)
         self.okButton = wx.Button(self, wx.ID_OK, "Save")
         self.cancelButton = wx.Button(self, wx.ID_CANCEL, "")
         self.Bind(wx.EVT_BUTTON, self.OnExit, self.cancelButton)
         self.Bind(wx.EVT_BUTTON, self.OnSave, self.okButton)
         
+        '''
+            The following list determines whaich settings are shown.  
+            The dictionary key is the plugin name and the value is a list of setting names as found in the corresponding .csv file for that plugin.
+            
+            NOTE:  Skeinforge is tightly integrated with Tkinter and there appears to be a dependency which stops radio-button values from being saved.
+                   Perhaps this can be solved, but at the moment this dialog cannot modify radio button values.  One will have to use the main Skeinforge application.  
+        '''
         self.moduleSettingsMap = {
                                 'bottom':['Activate Bottom', 'Additional Height over Layer Thickness (ratio):'],
                                 'carve':['Layer Thickness (mm):', 'Perimeter Width over Thickness (ratio):'],
@@ -30,40 +39,47 @@ class SkeinforgeQuickEditDialog(wx.Dialog):
         self.settingsSizer = self.getProfileSettings()
         self.scrollbarPanel.SetSizer(self.settingsSizer)
 
-        self.__set_properties()        
-        self.__do_layout()
-        
+        self.__set_properties()   
+        self.__do_layout()        
         self.Show()
         
     def __set_properties(self):
         self.profileName = skeinforge_profile.getProfileName(skeinforge_profile.getCraftTypeName())
         self.SetTitle("Skeinforge Quick Edit Profile: " + self.profileName)
-        self.SetSize(wx.DLG_SZE(self, (574, 362)))
-        self.SetPosition((100, 100))
+        
+        # For some reason the dialog size is not consistent between Windows and Linux - this is a hack to get it working 
+        if (os.name == 'nt'):
+            self.SetMinSize(wx.DLG_SZE(self, (465, 370)))
+        else:
+            self.SetSize(wx.DLG_SZE(self, (465, 325)))
+            
+        self.SetPosition((0, 0))
         self.scrollbarPanel.SetScrollRate(10, 10)
         
     def __do_layout(self):
         mainSizer = wx.BoxSizer(wx.VERTICAL)
-
         actionsSizer = wx.BoxSizer(wx.HORIZONTAL)
-        
-        mainSizer.Add(self.scrollbarPanel, 1, wx.EXPAND|wx.ALL, 5)
+        mainSizer.Add(self.scrollbarPanel, 1, wx.EXPAND | wx.ALL, 5)
         actionsSizer.Add(self.okButton, 0, 0, 0)
         actionsSizer.Add(self.cancelButton, 0, wx.LEFT, 10)
-        mainSizer.Add(actionsSizer, 0, wx.ALL | wx.ALIGN_RIGHT, 5)
+        mainSizer.Add(actionsSizer, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
         self.SetSizer(mainSizer)
         self.Layout()
-        
+                
     def getProfileSettings(self):
-        settingsSizer = wx.GridBagSizer(hgap=2, vgap=2)        
+        settingsSizer = wx.GridBagSizer(hgap=2, vgap=2)
         settingsRow = 0
         
         for craftName in sorted(self.moduleSettingsMap.keys()):
             
             craftStaticBox = wx.StaticBox(self.scrollbarPanel, -1, craftName.capitalize())
             craftStaticBoxSizer = wx.StaticBoxSizer(craftStaticBox, wx.VERTICAL)
-            craftStaticBoxSizer.SetMinSize((420,-1))
             
+            # For some reason the dialog size is not consistent between Windows and Linux - this is a hack to get it working
+            if (os.name == 'nt'):
+                craftStaticBoxSizer.SetMinSize((320, -1))
+            else: 
+                craftStaticBoxSizer.SetMinSize((450, -1))
             pluginModule = archive.getModuleWithPath(os.path.join(skeinforge_craft.getPluginsDirectoryPath(), craftName))
             repo = pluginModule.getNewRepository()
             
@@ -74,6 +90,7 @@ class SkeinforgeQuickEditDialog(wx.Dialog):
                     settingSizer.AddGrowableCol(0)
                     settingRow = 0
                     settingLabel = wx.StaticText(self.scrollbarPanel, -1, setting.name)
+                    settingLabel.Wrap(400)
                     settingSizer.Add(settingLabel, pos=(settingRow, 0))
                     
                     if (isinstance(setting.value, bool)):
@@ -81,16 +98,16 @@ class SkeinforgeQuickEditDialog(wx.Dialog):
                         checkbox.SetName(craftName + '.' + setting.name)
                         checkbox.SetValue(setting.value)
                         settingSizer.Add(checkbox, pos=(settingRow, 1))
-                        settingSizer.AddSpacer((125,-1), pos=(settingRow, 2))
+                        settingSizer.AddSpacer((25, -1), pos=(settingRow, 2))
                     else:
-                        textCtrl = wx.TextCtrl(self.scrollbarPanel, value=str(setting.value), size=(140, -1))
+                        textCtrl = wx.TextCtrl(self.scrollbarPanel, value=str(setting.value), size=(50, -1))
                         textCtrl.SetName(craftName + '.' + setting.name)
                         settingSizer.Add(textCtrl, pos=(settingRow, 1))
                         
                     craftStaticBoxSizer.Add(settingSizer, 1, wx.EXPAND, 0)
                     settingRow += 1
             col = settingsRow % 2
-            settingsSizer.Add(craftStaticBoxSizer, pos=(settingsRow-col, col))
+            settingsSizer.Add(craftStaticBoxSizer, pos=(settingsRow - col, col))
             settingsRow += 1
 
         return settingsSizer
