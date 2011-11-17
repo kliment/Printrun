1e53117b7753c84755ee95b7dede8787ea7fde3c
Kliment Yanev <kliment.yanev@gmail.com>
2011-11-17 13:48:47 +0100

    OpenGL-based plater update (requires a pyglet install)

--- plater.py
+++ plater.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import wx,time,random,threading,os,math
 import stltool
+glview=True
+try:
+    import stlview
+except:
+    glview=False
 
 def translate(l): return l
 
@@ -22,14 +27,152 @@ class stlwrap:
 class showstl(wx.Window):
     def __init__(self,parent,size,pos):
         wx.Window.__init__(self,parent,size=size,pos=pos)
-        self.l=wx.ListBox(self,size=(300,180),pos=(0,size[1]-180))
-        self.cl=wx.Button(self,label="Clear",pos=(300,size[1]-180))
-        self.lb=wx.Button(self,label="Load",pos=(300,size[1]-155))
-        self.eb=wx.Button(self,label="Export",pos=(300,size[1]-130))
-        self.sb=wx.Button(self,label="Snap to Z=0",pos=(300,size[1]-105))
-        self.cb=wx.Button(self,label="Put at 100,100",pos=(300,size[1]-80))
-        self.db=wx.Button(self,label="Delete",pos=(300,size[1]-55))
-        self.ab=wx.Button(self,label="Auto",pos=(300,size[1]-30))
+        #self.SetBackgroundColour((0,0,0))
+        #wx.FutureCall(200,self.paint)
+        self.i=0
+        self.parent=parent
+        self.previ=0
+        self.Bind(wx.EVT_MOUSEWHEEL,self.rot)
+        self.Bind(wx.EVT_MOUSE_EVENTS,self.move)
+        self.Bind(wx.EVT_PAINT,self.repaint)
+        #self.s=stltool.stl("sphere.stl").scale([2,1,1])
+        self.triggered=0
+        self.initpos=None
+        self.prevsel=-1
+
+    def drawmodel(self,m,scale):
+        m.bitmap=wx.EmptyBitmap(800,800,32)
+        dc=wx.MemoryDC()
+        dc.SelectObject(m.bitmap)
+        dc.SetBackground(wx.Brush((0,0,0,0)))
+        dc.SetBrush(wx.Brush((0,0,0,255)))
+        #dc.DrawRectangle(-1,-1,10000,10000)
+        dc.SetBrush(wx.Brush(wx.Colour(128,255,128)))
+        dc.SetPen(wx.Pen(wx.Colour(128,128,128)))
+        #m.offsets=[10,10,0]
+        #print m.offsets,m.dims
+        for i in m.facets:#random.sample(m.facets,min(100000,len(m.facets))):
+            dc.DrawPolygon([wx.Point(400+scale*p[0],(400-scale*p[1])) for p in i[1]])
+            #if(time.time()-t)>5:
+            #    break
+        dc.SelectObject(wx.NullBitmap)
+        m.bitmap.SetMask(wx.Mask(m.bitmap,wx.Colour(0,0,0,255)))
+    
+            
+    def move(self,event):
+        if event.ButtonUp(wx.MOUSE_BTN_LEFT):
+            if(self.initpos is not None):
+                i=self.parent.l.GetSelection()
+                if i != wx.NOT_FOUND:
+                    p=event.GetPositionTuple()
+                    #print (p[0]-self.initpos[0]),(p[1]-self.initpos[1])
+                    t=time.time()
+                    m=self.parent.models[self.parent.l.GetString(i)]
+                    m.offsets=[m.offsets[0]+0.5*(p[0]-self.initpos[0]),m.offsets[1]-0.5*(p[1]-self.initpos[1]),m.offsets[2]]
+                    #self.models[self.l.GetItemText(i)]=self.models[self.l.GetItemText(i)].translate([0.5*(p[0]-self.initpos[0]),0.5*(p[1]-self.initpos[1]),0])
+                    #print time.time()-t
+                self.Refresh()
+                self.initpos=None
+        elif event.ButtonDown(wx.MOUSE_BTN_RIGHT):
+            self.parent.right(event)
+        elif event.Dragging():
+            if self.initpos is None:
+                self.initpos=event.GetPositionTuple()
+            self.Refresh()
+            dc=wx.ClientDC(self)
+            p=event.GetPositionTuple()
+            dc.DrawLine(self.initpos[0],self.initpos[1],p[0],p[1])
+            #print math.sqrt((p[0]-self.initpos[0])**2+(p[1]-self.initpos[1])**2)
+                    
+            del dc
+        else:
+            event.Skip()
+        
+    def rotateafter(self):
+        if(self.i!=self.previ):
+            i=self.parent.l.GetSelection()
+            if i != wx.NOT_FOUND:
+                #o=self.models[self.l.GetItemText(i)].offsets
+                self.parent.models[self.parent.l.GetString(i)].rot+=5*(self.i-self.previ)
+                #self.models[self.l.GetItemText(i)].offsets=o
+            self.previ=self.i
+            self.Refresh()
+    def cr(self):
+        time.sleep(0.01)
+        wx.CallAfter(self.rotateafter)
+        self.triggered=0
+        
+    def rot(self, event):
+        z=event.GetWheelRotation()
+        s=self.parent.l.GetSelection()
+        if self.prevsel!=s:
+            self.i=0
+            self.prevsel=s
+        if z > 0:
+            self.i-=1
+        else:
+            self.i+=1
+        if not self.triggered:
+            self.triggered=1
+            threading.Thread(target=self.cr).start()
+    
+    def repaint(self,event):
+        dc=wx.PaintDC(self)
+        self.paint(dc=dc)
+        
+    def paint(self,coord1="x",coord2="y",dc=None):
+        coords={"x":0,"y":1,"z":2}
+        if dc is None:
+            dc=wx.ClientDC(self)
+        offset=[0,0]
+        scale=2
+        dc.SetPen(wx.Pen(wx.Colour(100,100,100)))
+        for i in xrange(20):
+            dc.DrawLine(0,i*scale*10,400,i*scale*10)
+            dc.DrawLine(i*scale*10,0,i*scale*10,400)
+        dc.SetPen(wx.Pen(wx.Colour(0,0,0)))
+        for i in xrange(4):
+            dc.DrawLine(0,i*scale*50,400,i*scale*50)
+            dc.DrawLine(i*scale*50,0,i*scale*50,400)
+        dc.SetBrush(wx.Brush(wx.Colour(128,255,128)))
+        dc.SetPen(wx.Pen(wx.Colour(128,128,128)))
+        t=time.time()
+        dcs=wx.MemoryDC()
+        for m in self.parent.models.values():
+            b=m.bitmap
+                #print b
+            im=b.ConvertToImage()
+                #print im
+            imgc = wx.Point( im.GetWidth()/2,im.GetHeight()/2 )
+                #print math.radians(5*(self.i-self.previ))
+            im= im.Rotate( math.radians(m.rot), imgc, 0)
+            bm=wx.BitmapFromImage(im)
+            dcs.SelectObject(bm)
+            bsz=bm.GetSize()
+            dc.Blit(scale*m.offsets[0]-bsz[0]/2,400-(scale*m.offsets[1]+bsz[1]/2),bsz[0],bsz[1],dcs,0,0,useMask=1)
+            #for i in m.facets:#random.sample(m.facets,min(100000,len(m.facets))):
+            #    dc.DrawPolygon([wx.Point(offset[0]+scale*m.offsets[0]+scale*p[0],400-(offset[1]+scale*m.offsets[1]+scale*p[1])) for p in i[1]])
+                #if(time.time()-t)>5:
+                #    break
+        del dc
+        #print time.time()-t
+        #s.export()
+        
+class stlwin(wx.Frame):
+    def __init__(self,size=(800,580)):
+        wx.Frame.__init__(self,None,title="Right-click to add a file",size=size)
+        self.SetIcon(wx.Icon("plater.ico",wx.BITMAP_TYPE_ICO))
+        self.mainsizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.panel=wx.Panel(self,-1,size=(150,600),pos=(0,0))
+        self.panel.SetBackgroundColour((10,10,10))
+        self.l=wx.ListBox(self.panel,size=(300,180),pos=(0,30))
+        self.cl=wx.Button(self.panel,label="Clear",pos=(0,205))
+        self.lb=wx.Button(self.panel,label="Load",pos=(0,0))
+        self.eb=wx.Button(self.panel,label="Export",pos=(100,0))
+        self.sb=wx.Button(self.panel,label="Snap to Z=0",pos=(00,255))
+        self.cb=wx.Button(self.panel,label="Put at 100,100",pos=(0,280))
+        self.db=wx.Button(self.panel,label="Delete",pos=(0,305))
+        self.ab=wx.Button(self.panel,label="Auto",pos=(0,330))
         self.cl.Bind(wx.EVT_BUTTON,self.clear)
         self.lb.Bind(wx.EVT_BUTTON,self.right)
         self.eb.Bind(wx.EVT_BUTTON,self.export)
@@ -37,20 +180,69 @@ class showstl(wx.Window):
         self.cb.Bind(wx.EVT_BUTTON,self.center)
         self.db.Bind(wx.EVT_BUTTON,self.delete)
         self.ab.Bind(wx.EVT_BUTTON,self.autoplate)
-        #self.SetBackgroundColour((0,0,0))
-        #wx.FutureCall(200,self.paint)
-        self.i=0
-        self.previ=0
-        self.Bind(wx.EVT_MOUSEWHEEL,self.rot)
-        self.Bind(wx.EVT_MOUSE_EVENTS,self.move)
-        self.Bind(wx.EVT_PAINT,self.repaint)
-        #self.s=stltool.stl("sphere.stl").scale([2,1,1])
-        self.triggered=0
-        self.models={}
         self.basedir="."
-        self.initpos=None
-        self.prevsel=-1
-
+        self.models={}
+        self.SetBackgroundColour((10,10,10))
+        self.mainsizer.Add(self.panel)
+        #self.mainsizer.AddSpacer(10)
+        if glview:
+            self.s=stlview.TestGlPanel(self,(580,580))
+        else:
+            self.s=showstl(self,(580,580),(0,0))
+        self.mainsizer.Add(self.s, 1, wx.EXPAND)
+        self.SetSizer(self.mainsizer)
+        #self.mainsizer.Fit(self)
+        self.Layout()
+        
+        #self.SetClientSize(size)
+    
+    def autoplate(self,event):
+        print "Autoplating"
+        separation = 2
+        bedsize = [200,200,100]
+        cursor = [0,0,0]
+        newrow = 0
+        max = [0,0]
+        for i in self.models:
+            self.models[i].offsets[2]=-1.0*self.models[i].dims[4]
+            x = abs(self.models[i].dims[0] - self.models[i].dims[1])
+            y = abs(self.models[i].dims[2] - self.models[i].dims[3])
+            centre = [x/2, y/2]
+            centreoffset = [self.models[i].dims[0] + centre[0], self.models[i].dims[2] + centre[1]]
+            if (cursor[0]+x+separation) >= bedsize[0]:
+                cursor[0] = 0
+                cursor[1] += newrow+separation
+                newrow = 0
+            if (newrow == 0) or (newrow < y):
+                newrow = y
+            #To the person who works out why the offsets are applied differently here:
+            # Good job, it confused the hell out of me.
+            self.models[i].offsets[0] = cursor[0] + centre[0] - centreoffset[0]
+            self.models[i].offsets[1] = cursor[1] + centre[1] - centreoffset[1]
+            if (max[0] == 0) or (max[0] < (cursor[0]+x)):
+                max[0] = cursor[0]+x
+            if (max[1] == 0) or (max[1] < (cursor[1]+x)):
+                max[1] = cursor[1]+x
+            cursor[0] += x+separation
+            if (cursor[1]+y) >= bedsize[1]:
+                print "Bed full, sorry sir :("
+                self.Refresh()
+                return
+        centreoffset = [(bedsize[0]-max[0])/2,(bedsize[1]-max[1])/2]
+        for i in self.models:
+            self.models[i].offsets[0] += centreoffset[0]
+            self.models[i].offsets[1] += centreoffset[1]
+        self.Refresh()
+    
+        
+    def clear(self,event):
+        result = wx.MessageBox('Are you sure you want to clear the grid? All unsaved changes will be lost.', 'Clear the grid?', 
+            wx.YES_NO | wx.ICON_QUESTION)
+        if (result == 2):
+            self.models={}
+            self.l.Clear()
+            self.Refresh()
+    
     def center(self,event):
         i=self.l.GetSelection()
         if i != -1:
@@ -90,50 +282,12 @@ class showstl(wx.Window):
                 if r != 0:
                     i=i.rotate([0,0,-r])
                 if o != [0,0,0]:
-                    i=i.translate([o[0],-o[1],o[2]])
+                    i=i.translate([o[0],o[1],o[2]])
                 facets+=i.facets
             sf.close()
             stltool.emitstl(name,facets,"plater_export")
             print "wrote ",name
             
-    def autoplate(self,event):
-        print "Autoplating"
-        separation = 2
-        bedsize = [200,200,100]
-        cursor = [0,0,0]
-        newrow = 0
-        max = [0,0]
-        for i in self.models:
-            self.models[i].offsets[2]=-1.0*self.models[i].dims[4]
-            x = abs(self.models[i].dims[0] - self.models[i].dims[1])
-            y = abs(self.models[i].dims[2] - self.models[i].dims[3])
-            centre = [x/2, y/2]
-            centreoffset = [self.models[i].dims[0] + centre[0], self.models[i].dims[2] + centre[1]]
-            if (cursor[0]+x+separation) >= bedsize[0]:
-                cursor[0] = 0
-                cursor[1] += newrow+separation
-                newrow = 0
-            if (newrow == 0) or (newrow < y):
-                newrow = y
-            #To the person who works out why the offsets are applied differently here:
-            # Good job, it confused the hell out of me.
-            self.models[i].offsets[0] = cursor[0] + centre[0] - centreoffset[0]
-            self.models[i].offsets[1] = cursor[1] + centre[1] + centreoffset[1]
-            if (max[0] == 0) or (max[0] < (cursor[0]+x)):
-                max[0] = cursor[0]+x
-            if (max[1] == 0) or (max[1] < (cursor[1]+x)):
-                max[1] = cursor[1]+x
-            cursor[0] += x+separation
-            if (cursor[1]+y) >= bedsize[1]:
-                print "Bed full, sorry sir :("
-                self.Refresh()
-                return
-        centreoffset = [(bedsize[0]-max[0])/2,(bedsize[1]-max[1])/2]
-        for i in self.models:
-            self.models[i].offsets[0] += centreoffset[0]
-            self.models[i].offsets[1] += centreoffset[1]
-        self.Refresh()
-    
     def right(self,event):
         dlg=wx.FileDialog(self,"Pick file to load",self.basedir,style=wx.FD_OPEN|wx.FD_FILE_MUST_EXIST)
         dlg.SetWildcard("STL files (;*.stl;)|*.stl|OpenSCAD files (;*.scad;)|*.scad")
@@ -178,7 +332,7 @@ class showstl(wx.Window):
         self.Refresh()
         #print time.time()-t
 
-    def load_stl_into_model(self,path,name,offset=[0,0,0],rotation=0):
+    def load_stl_into_model(self,path,name,offset=[0,0,0],rotation=0,scale=[1.0,1.0,1.0]):
         newname=os.path.split(name.lower())[1]
         c=1
         while newname in self.models:
@@ -188,6 +342,7 @@ class showstl(wx.Window):
         self.models[newname]=stltool.stl(path)
         self.models[newname].offsets=offset
         self.models[newname].rot=rotation
+        self.models[newname].scale=scale
         self.models[newname].filename=name
         minx,miny,minz,maxx,maxy,maxz=(10000,10000,10000,0,0,0)
         for i in self.models[newname].facets:
@@ -209,24 +364,7 @@ class showstl(wx.Window):
         #    self.models[newname].offsets[0]=-minx
         #if miny<0:
         #    self.models[newname].offsets[1]=-miny
-        self.models[newname].bitmap=wx.EmptyBitmap(800,800,32)
-        dc=wx.MemoryDC()
-        dc.SelectObject(self.models[newname].bitmap)
-        dc.SetBackground(wx.Brush((0,0,0,0)))
-        dc.SetBrush(wx.Brush((0,0,0,255)))
-        #dc.DrawRectangle(-1,-1,10000,10000)
-        dc.SetBrush(wx.Brush(wx.Colour(128,255,128)))
-        dc.SetPen(wx.Pen(wx.Colour(128,128,128)))
-        m=self.models[newname]
-        #m.offsets=[10,10,0]
-        print m.offsets,m.dims
-        scale=2
-        for i in m.facets:#random.sample(m.facets,min(100000,len(m.facets))):
-            dc.DrawPolygon([wx.Point(400+scale*p[0],(400+scale*p[1])) for p in i[1]])
-            #if(time.time()-t)>5:
-            #    break
-        dc.SelectObject(wx.NullBitmap)
-        m.bitmap.SetMask(wx.Mask(m.bitmap,wx.Colour(0,0,0,255)))
+        self.s.drawmodel(self.models[newname],2)
         
         #print time.time()-t
         self.l.Append(newname)
@@ -236,120 +374,6 @@ class showstl(wx.Window):
     
         self.l.Select(self.l.GetCount()-1)
     
-    def clear(self,event):
-        result = wx.MessageBox('Are you sure you want to clear the grid? All unsaved changes will be lost.', 'Clear the grid?', 
-            wx.YES_NO | wx.ICON_QUESTION)
-        if (result == 2):
-            self.models={}
-            self.l.Clear()
-            self.Refresh()
-            
-    def move(self,event):
-        if event.ButtonUp(wx.MOUSE_BTN_LEFT):
-            if(self.initpos is not None):
-                i=self.l.GetSelection()
-                if i != wx.NOT_FOUND:
-                    p=event.GetPositionTuple()
-                    #print (p[0]-self.initpos[0]),(p[1]-self.initpos[1])
-                    t=time.time()
-                    m=self.models[self.l.GetString(i)]
-                    m.offsets=[m.offsets[0]+0.5*(p[0]-self.initpos[0]),m.offsets[1]-0.5*(p[1]-self.initpos[1]),m.offsets[2]]
-                    #self.models[self.l.GetItemText(i)]=self.models[self.l.GetItemText(i)].translate([0.5*(p[0]-self.initpos[0]),0.5*(p[1]-self.initpos[1]),0])
-                    #print time.time()-t
-                self.Refresh()
-                self.initpos=None
-        elif event.ButtonDown(wx.MOUSE_BTN_RIGHT):
-            self.right(event)
-        elif event.Dragging():
-            if self.initpos is None:
-                self.initpos=event.GetPositionTuple()
-            self.Refresh()
-            dc=wx.ClientDC(self)
-            p=event.GetPositionTuple()
-            dc.DrawLine(self.initpos[0],self.initpos[1],p[0],p[1])
-            #print math.sqrt((p[0]-self.initpos[0])**2+(p[1]-self.initpos[1])**2)
-                    
-            del dc
-        else:
-            event.Skip()
-        
-        
-    def rotateafter(self):
-        if(self.i!=self.previ):
-            i=self.l.GetSelection()
-            if i != wx.NOT_FOUND:
-                #o=self.models[self.l.GetItemText(i)].offsets
-                self.models[self.l.GetString(i)].rot+=5*(self.i-self.previ)
-                #self.models[self.l.GetItemText(i)].offsets=o
-            self.previ=self.i
-            self.Refresh()
-    def cr(self):
-        time.sleep(0.01)
-        wx.CallAfter(self.rotateafter)
-        self.triggered=0
-        
-    def rot(self, event):
-        z=event.GetWheelRotation()
-        s=self.l.GetSelection()
-        if self.prevsel!=s:
-            self.i=0
-            self.prevsel=s
-        if z > 0:
-            self.i-=1
-        else:
-            self.i+=1
-        if not self.triggered:
-            self.triggered=1
-            threading.Thread(target=self.cr).start()
-    
-    def repaint(self,event):
-        dc=wx.PaintDC(self)
-        self.paint(dc=dc)
-        
-    def paint(self,coord1="x",coord2="y",dc=None):
-        coords={"x":0,"y":1,"z":2}
-        if dc is None:
-            dc=wx.ClientDC(self)
-        offset=[0,0]
-        scale=2
-        dc.SetPen(wx.Pen(wx.Colour(100,100,100)))
-        for i in xrange(20):
-            dc.DrawLine(0,i*scale*10,400,i*scale*10)
-            dc.DrawLine(i*scale*10,0,i*scale*10,400)
-        dc.SetPen(wx.Pen(wx.Colour(0,0,0)))
-        for i in xrange(4):
-            dc.DrawLine(0,i*scale*50,400,i*scale*50)
-            dc.DrawLine(i*scale*50,0,i*scale*50,400)
-        dc.SetBrush(wx.Brush(wx.Colour(128,255,128)))
-        dc.SetPen(wx.Pen(wx.Colour(128,128,128)))
-        t=time.time()
-        dcs=wx.MemoryDC()
-        for m in self.models.values():
-            b=m.bitmap
-                #print b
-            im=b.ConvertToImage()
-                #print im
-            imgc = wx.Point( im.GetWidth()/2,im.GetHeight()/2 )
-                #print math.radians(5*(self.i-self.previ))
-            im= im.Rotate( math.radians(m.rot), imgc, 0)
-            bm=wx.BitmapFromImage(im)
-            dcs.SelectObject(bm)
-            bsz=bm.GetSize()
-            dc.Blit(scale*m.offsets[0]-bsz[0]/2,400-(scale*m.offsets[1]+bsz[1]/2),bsz[0],bsz[1],dcs,0,0,useMask=1)
-            #for i in m.facets:#random.sample(m.facets,min(100000,len(m.facets))):
-            #    dc.DrawPolygon([wx.Point(offset[0]+scale*m.offsets[0]+scale*p[0],400-(offset[1]+scale*m.offsets[1]+scale*p[1])) for p in i[1]])
-                #if(time.time()-t)>5:
-                #    break
-        del dc
-        #print time.time()-t
-        #s.export()
-        
-class stlwin(wx.Frame):
-    def __init__(self,size=(400,580)):
-        wx.Frame.__init__(self,None,title="Right-click to add a file",size=size)
-        self.SetIcon(wx.Icon("plater.ico",wx.BITMAP_TYPE_ICO))
-        self.SetClientSize(size)
-        self.s=showstl(self,(400,580),(0,0))
         
 if __name__ == '__main__':
     app = wx.App(False)
