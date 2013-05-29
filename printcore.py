5ddea3cc6e301f79b2e199f44e81841ce83030b2
Guillaume Seguin <guillaume@segu.in>
2013-05-29 10:56:44 +0200

    Fix typo in printcore.py

--- printcore.py
+++ printcore.py
@@ -414,7 +414,7 @@ class printcore():
             (layer, line) = self.mainqueue.idxs(self.queueindex)
             gline = self.mainqueue.all_layers[layer].lines[line]
             if self.layerchangecb and self.queueindex > 0:
-                (prev_layer, prev_line) = self.mainqueue.idxs(self.queuindex - 1)
+                (prev_layer, prev_line) = self.mainqueue.idxs(self.queueindex - 1)
                 if prev_layer != layer:
                     self.layerchangecb(layer)
             tline = gline.raw
