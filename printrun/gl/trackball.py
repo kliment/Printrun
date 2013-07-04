f4525e45334bd02d3362467f6beb3c7d692ddd0b
Guillaume Seguin <guillaume@segu.in>
2013-07-04 17:53:38 +0200

    Fix missing import in printrun.gl.trackball

--- printrun/gl/trackball.py
+++ printrun/gl/trackball.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Printrun.  If not, see <http://www.gnu.org/licenses/>.
 
+import math
+
 from pyglet.gl import *
 
 def cross(v1, v2):
