82251510ce401624092e3cf71629b65c9688e6ab
Guillaume Seguin <guillaume@segu.in>
2013-08-08 22:44:40 +0200

    Detect Python3 nicely

--- pronterface.py
+++ pronterface.py
@@ -15,6 +15,20 @@
 # You should have received a copy of the GNU General Public License
 # along with Printrun.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
+try:
+    import wx
+except:
+    print("wxPython is not installed. This program requires wxPython to run.")
+    if sys.version_info.major >= 3:
+        print("""\
+As you are currently running python3, this is most likely because wxPython is
+not yet available for python3. You should try running with python2 instead.""")
+        sys.exit(-1)
+    else:
+        raise
+
 from printrun.pronterface import PronterApp
 
 if __name__ == '__main__':
