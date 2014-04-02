2cb00a4c95af55a16def45e89ba2dee4b1bce7fa
Guillaume Seguin <guillaume@segu.in>
2014-04-02 18:50:11 +0200

    Fix accentuated filenames handling in platers

--- printrun/objectplater.py
+++ printrun/objectplater.py
@@ -237,6 +237,7 @@ class Plater(wx.Frame):
 
     def add_model(self, name, model):
         newname = os.path.split(name.lower())[1]
+        newname = unicode(newname, "utf-8")
         c = 1
         while newname in self.models:
             newname = os.path.split(name.lower())[1]
