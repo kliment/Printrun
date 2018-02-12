06e3af45a981f7d67e5a52665c796dedfa73f7bc
Miro Hrončok <miro@hroncok.cz>
2018-02-12 16:41:57 +0100

    Python 3: Fix shebangs
     * convert to python3
     * only have shebangs + exec bits in files that have something to execute

--- printrun/eventhandler.py
+++ printrun/eventhandler.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # This file is part of the Printrun suite.
 #
 # Printrun is free software: you can redistribute it and/or modify
--- printrun/gcoder.py
+++ printrun/gcoder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # This file is copied from GCoder.
 #
 # GCoder is free software: you can redistribute it and/or modify
