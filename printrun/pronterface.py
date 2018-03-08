72103d1ace7f36e5cc2362e689c5f5967e3a437a
siorai <siorai@gmail.com>
2018-03-08 01:02:09 -0800

    Updated copyright.

--- printrun/pronterface.py
+++ printrun/pronterface.py
@@ -825,7 +825,7 @@ class PronterWindow(MainWindow, pronsole.pronsole):
                        % self.settings.total_filament_used
 
         info.SetDescription(description)
-        info.SetCopyright('(C) 2011 - 2015')
+        info.SetCopyright('(C) 2011 - 2018')
         info.SetWebSite('https://github.com/kliment/Printrun')
 
         licence = """\
