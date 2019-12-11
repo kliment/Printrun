3d6b3cacb1b71f6394b7695df534ced9247313bf
volconst <20997907+volconst@users.noreply.github.com>
2019-12-11 21:59:43 +0200

    Implement M420 parsing

--- printrun/gsync.py
+++ printrun/gsync.py
@@ -21,6 +21,7 @@ import threading
 import printcore
 import numpy as np
 
+verbose = False
 class MyHandler(SampleHandler):
     def __init__(self):
         SampleHandler.__init__(self)
@@ -28,11 +29,12 @@ class MyHandler(SampleHandler):
     def clear(self):
         self.printer_response = []
     def on_recv(self, ln):
-        #print('MyHandler.on_recv', ln);
         self.printer_response.append(ln)
         if ln.startswith('ok'):
             with ok_received:
                 ok_received.notify()
+        elif verbose:
+            print('>', ln, end='');
 
 def connect(tty, baud):
     global p, h
@@ -79,16 +81,64 @@ class M114Class(SyncCommand):
             m = re.match('X:([\d.-]+)\sY:([\d.-]+)\sZ:([\d.-]+)', ln)
             if m:
                 self.x, self.y, self.z = [float(f) for f in m.groups()]
+class M420Class(SyncCommand):
+    def __init__(self):
+        SyncCommand.__init__(self,True)
+    def parseResponse(self, lines):
+        self.leveling_enabled = None
+        dc = {}
+        max_j = -1
+        for ln in lines:
+            #UBL:
+            #  2 | +0.500  +0.400   0.000
+            #  0 |[+0.170] +0.370  +0.100
+            #ABL:
+            # 0  =====  =====  =====
+            m = re.match('\s+([\d]+)\s*\|?([ \[+\d.\]=-]+)', ln)
+            if m:
+                j = int(m.group(1))
+                max_j = max(max_j, j)
+                dc[j] = [float(z.strip('[]').replace('=====', 'nan')) for z in m.group(2).split()]
+            #echo:Bed Leveling On
+            else:
+                m = re.match('echo:Bed Leveling (On|Off)', ln)
+                if m:
+                    self.leveling_enabled = m.group(1) == 'On'
+        self.topology = [None] * (max_j + 1)
+        for j, r in dc.items():
+            self.topology[j] = r
+        if self.leveling_enabled is None:
+            assert False, 'Did not find bed status'
 class M503Class(SyncCommand):
     def __init__(self):
         SyncCommand.__init__(self,True)
         self.hasUBL = False
     def parseResponse(self, lines):
-        self.hasUBL = False
+        self.hasUBL = self.hasABL = False
+        z_map = {}
+        max_i = max_j = -1
         for ln in lines:
             if ln.startswith('Unified Bed Leveling'):
                 self.hasUBL = True
-                break
+            elif ln.startswith('echo:Auto Bed Leveling:'):
+                self.hasABL = True
+            else:
+                #echo:  G29 W I1 J2 Z0.00000
+                #print(ln)
+                m = re.match('echo:  G29 W I([\d]+) J([\d]+) Z([\d.-]+)', ln)
+                #print('m', m)
+                if m:
+                    i = int(m.group(1))
+                    j = int(m.group(2))
+                    z_map[i,j] = float(m.group(3))
+                    max_i = max(max_i, i)
+                    max_j = max(max_j, j)
+        self.topology = [[None] * (max_i + 1) for j in range(max_j + 1)]
+        #print('self.topology', z_map, self.topology)
+        for (i, j), z in z_map.items():
+            #print(i, j, z)
+            self.topology[j][i] = z
+
 class G29WClass(SyncCommand):
     def __init__(self):
         SyncCommand.__init__(self, True)
@@ -111,7 +161,27 @@ class G29WClass(SyncCommand):
         SyncCommand.send(self, 'W')
     def name(self):
         return 'G29'
+class G42Class(SyncCommand):
+    def __init__(self):
+        SyncCommand.__init__(self, True)
+    def parseResponse(self, lines):
+        for ln in lines:
+            m = re.match('(GRID_MAX_POINTS_[XY])\s+([\d]+)', ln)
+            if m:
+                setattr(self, m.group(1), int(m.group(2)))
+                continue
+            #X-Axis Mesh Points at: 0.000  150.000  300.000
+            m = re.match('[XY]-Axis Mesh Points at:', ln)
+            if m:
+                pts = ln.split(':')[1]
+                pts = np.array([float(pt) for pt in pts.split()])
+                if ln[0] == 'X':
+                    self.x_mesh_points = pts
+                else:
+                    self.y_mesh_points = pts
+    
 M114 = M114Class()
+M420 = M420Class()
 M503 = M503Class()
 G29W = G29WClass()
 
