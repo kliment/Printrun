15e27dc32b681d5d345909edf8aad61c871a892f
DivingDuck <DivingDuck@users.noreply.github.com>
2020-07-07 19:20:59 +0200

    Remove outdated commands, need rework. Usecase for now: copy all translations and images to ./dist

--- release_windows.bat
+++ release_windows.bat
@@ -1,6 +1,8 @@
-d:\python27\python.exe setup_win.py py2exe -v
+rem AGe ** this batch is outdated, use pyinstall instead, need a bit more love somewhen later **
+rem AGe, not needed for now: d:\python27\python.exe setup_win.py py2exe -v
+rem AGe, not needed for now: py -3.7 setup_win.py py2exe -v
 xcopy images dist\images\ /Y /E
 xcopy locale dist\locale\ /Y /E
-xcopy Slic3r dist\Slic3r\ /Y /E
-copy MSVCP90.DLL dist\
+rem AGe,, not needed for now: xcopy Slic3r dist\Slic3r\ /Y /E
+rem AGe, not needed for now: copy MSVCP90.DLL dist\ 
 pause
