2d35b6dd2e5eb03d20879b60fd3c7af13a1b93ef
DivingDuck <DivingDuck@users.noreply.github.com>
2021-02-11 17:52:31 +0100

    Update for Projector

--- release_windows.bat
+++ release_windows.bat
@@ -1,40 +1,47 @@
 echo off
 cls
 
-rem *************************************************************************
-rem ****************  ---> New batch file starts here <---  *****************
-rem **                                                                     **
-rem **  This batch will compile automated via command line an executable   **
-rem **  Pronterface file for Windows 10.                                   **
-rem **                                                                     **
-rem **  Steps that are automated:                                          **
-rem **                                                                     **
-rem **  1. clean up previous compilations (directory .\dist)               **
-rem **  2. check for virtual environment called v3 and generate it, if     **
-rem **     not available (start from scratch)                              **
-rem **  3. install all needed additional modules via pip                   **
-rem **  4. check for outdated modules that need to be updated and          **
-rem **     update them                                                     **
-rem **  5. Check if virtual environment needs an update and do it          **
-rem **  6. check for existing variants of gcoder_line.cp??-win_amd??.pyd   **
-rem **     and delete them (to prevent errors and incompatibilities)       **
-rem **  7. compile Pronterface.exe                                         **
-rem **  8. copy localization files to .\dist                               **
-rem **  9. go to directory .\dist, list files and ends the activity        **
-rem **                                                                     **
-rem **  Steps, you need to do manually before running this batch:          **
-rem **                                                                     **
-rem **  1. install python 3.7.9                                            **
-rem **     https://www.python.org/downloads/release/python-378/            **
-rem **  2. install C-compiler environment                                  **
-rem **     https://wiki.python.org/moin/WindowsCompilers                   **
-rem **  3. check for latest repository updates at:                         **
-rem **     http://github.com/kliment/Printrun.git                          **
-rem **                                                                     **
-rem **  Author: DivingDuck, 2021-02-10, Status: working                    **
-rem **                                                                     **
-rem *************************************************************************
-rem *************************************************************************
+rem ************************************************************************************
+rem *********************  ---> New batch file starts here <---  ***********************
+rem **                                                                                **
+rem **  This batch will compile automated via command line an executable              **
+rem **  Pronterface file for Windows 10.                                              **
+rem **                                                                                **
+rem **  Steps that are automated:                                                     **
+rem **                                                                                **
+rem **  1. clean up previous compilations (directory .\dist)                          **
+rem **  2. check for virtual environment called v3 and generate it, if                **
+rem **     not available (start from scratch)                                         **
+rem **  3. install all needed additional modules via pip                              **
+rem **  4. check for outdated modules that need to be updated and                     **
+rem **     update them                                                                **
+rem **  5. Check if virtual environment needs an update and do it                     **
+rem **  6. check for existing variants of gcoder_line.cp??-win_amd??.pyd              **
+rem **     and delete them (to prevent errors and incompatibilities)                  **
+rem **  7. compile Pronterface.exe                                                    **
+rem **  8. copy localization files to .\dist                                          **
+rem **  9. go to directory .\dist, list files and ends the activity                   **
+rem **                                                                                **
+rem **  Steps, you need to do manually before running this batch:                     **
+rem **                                                                                **
+rem **  1. install python 3.7.9                                                       **
+rem **     https://www.python.org/downloads/release/python-378/                       **
+rem **  2. install C-compiler environment                                             **
+rem **     https://wiki.python.org/moin/WindowsCompilers                              **
+rem **  3. check for latest repository updates at:                                    **
+rem **     http://github.com/kliment/Printrun.git                                     **
+rem **  4. Projector needs GTK+ for Windows Runtime Environment installed.            **
+rem **     There are different compilations, depending on the installed               **
+rem **     Windows Version, available. Follow the instructions from:                  **
+rem **     https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer  **
+rem **     Add your GTK3 bineries installation path to system                         **
+rem **     environment variable Path (like Path=c:\GTK3\bin). You can find as         **
+rem **     reference a listing of all used cairo DLL's in file VERSION                **
+rem **                                                                                **
+rem **  Author: DivingDuck, 2021-02-11, Status: working                               **
+rem **                                                                                **
+rem ************************************************************************************
+rem ************************************************************************************
 
 echo **************************************************
 echo ****** Delete files and directory of .\dist ******
@@ -79,10 +86,6 @@ if exist v3 (
    pip install pyinstaller
    pip install pypiwin32
    pip install polygon3
-   rem For running Projector from source only
-   rem Alternatively install runtime library GTK3 works too:
-   rem https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases
-   pip install pycairo
    )
 
 echo ********************************************
@@ -110,7 +113,7 @@ python setup.py build_ext --inplace
 echo ****************************************
 echo ****** Collect all data for build ******
 echo ****************************************
-pyi-makespec -F --add-data images/*;images --add-data *.png;. --add-data *.ico;. -w -i pronterface.ico pronterface.py 
+pyi-makespec -F --add-data VERSION;cairocffi --add-data VERSION;cairosvg --add-data images/*;images --add-data *.png;. --add-data *.ico;. -w -i pronterface.ico pronterface.py
 
 echo *******************************
 echo ****** Build Pronterface ******
