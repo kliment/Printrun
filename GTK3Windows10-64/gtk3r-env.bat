4bbebc28421a7d4863678a0e33367669269c2d58
DivingDuck <DivingDuck@users.noreply.github.com>
2021-02-14 21:51:57 +0100

    delete local GTK files

--- GTK3Windows10-64/gtk3r-env.bat
+++
@@ -1,9 +0,0 @@
-@set GTK3R_PREFIX=C:\GTK3-RuntimeWin64\bin
-
-@echo Setting environment variables for GTK3-Runtime.
-@echo.
-
-@echo set PATH=%GTK3R_PREFIX%;%%PATH%%
-@set PATH=%GTK3R_PREFIX%;%PATH%
-
-@echo.
