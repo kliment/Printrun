a5091d93ebe963beff134a02376eb1ac129815c8
Rock Storm <rockstorm@gmx.com>
2024-01-04 13:18:36 +0100

    p/eventhandler: Re-document all functions

--- printrun/eventhandler.py
+++ printrun/eventhandler.py
@@ -12,115 +12,85 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Printrun.  If not, see <http://www.gnu.org/licenses/>.
+#
+# pylint: disable=unnecessary-pass
+# pylint: disable=missing-function-docstring
 
 class PrinterEventHandler:
-    '''
-    Defines a skeletton of an event-handler for printer events. It
-    allows attaching to the printcore and will be triggered for
-    different events.
-    '''
+    """Skeleton of an event-handler for printer events.
+
+    It allows attaching to a printcore instance and the relevant method will
+    be triggered for different events. See
+    `printrun.printcore.printcore.addEventHandler`
+
+    Event method `on_send` will be called at the same time and with the same
+    arguments as the printcore `send` callback function. See
+    `printrun.printcore.Callback`. Same logic applies to all other methods
+    but for `on_preprintsend`, `on_init`, `on_connect` and
+    `on_disconnect`. See below.
+
+    """
     def __init__(self):
-        '''
-        Constructor.
-        '''
+        """Event-handler constructor."""
         pass
-    
+
     def on_init(self):
-        '''
-        Called whenever a new printcore is initialized.
-        '''
+        """Called whenever a new printcore is initialized."""
         pass
-    
+
     def on_send(self, command, gline):
-        '''
-        Called on every command sent to the printer.
-        
-        @param command: The command to be sent.
-        @param gline: The parsed high-level command.
-        '''
         pass
-    
+
     def on_recv(self, line):
-        '''
-        Called on every line read from the printer.
-        
-        @param line: The data has been read from printer.
-        '''
         pass
-    
-    
+
     def on_connect(self):
-        '''
-        Called whenever printcore is connected.
-        '''
+        """Called whenever printcore is connected."""
         pass
-    
+
     def on_disconnect(self):
-        '''
-        Called whenever printcore is disconnected.
-        '''
+        """Called whenever printcore is disconnected."""
         pass
-    
+
     def on_error(self, error):
-        '''
-        Called whenever an error occurs.
-        
-        @param error: The error that has been triggered.
-        '''
         pass
-    
+
     def on_online(self):
-        '''
-        Called when printer got online.
-        '''
         pass
-    
+
     def on_temp(self, line):
-        '''
-        Called for temp, status, whatever.
-        
-        @param line: Line of data.
-        '''
         pass
-    
+
     def on_start(self, resume):
-        '''
-        Called when printing is started.
-        
-        @param resume: If true, the print is resumed.
-        '''
         pass
-    
+
     def on_end(self):
-        '''
-        Called when printing ends.
-        '''
         pass
-    
+
     def on_layerchange(self, layer):
-        '''
-        Called on layer changed.
-        
-        @param layer: The new layer.
-        '''
         pass
-    
+
     def on_preprintsend(self, gline, index, mainqueue):
-        '''
-        Called pre sending printing command.
-        
-        @param gline: Line to be send.
-        @param index: Index in the mainqueue.
-        @param mainqueue: The main queue of commands.
-        '''
+        """Called before sending each command of a print.
+
+        This event is only triggered on lines sent while a print is ongoing.
+        See `printrun.printcore.printcore.startprint`.
+
+        Parameters
+        ----------
+        gline : Line
+            The `printrun.gcoder.Line` object containing the line of G-code to
+            be sent.
+        index : int
+            Index of this `gline` within `mainqueue`.
+        mainqueue : GCode
+            A `printrun.gcoder.GCode` object with the current queue of
+            commands being processed.
+            See `printrun.printcore.printcore.mainqueue`.
+
+        """
+        # TODO[v3]: Rework to match callback function arguments?
         pass
-    
+
     def on_printsend(self, gline):
-        '''
-        Called whenever a line is sent to the printer.
-        
-        @param gline: The line send to the printer.
-        '''
         pass
-        
-        
