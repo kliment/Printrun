96f80ef90a3431526d9a20b95918be85da796049
DivingDuck <DivingDuck@users.noreply.github.com>
2025-02-16 16:52:02 +0100

    Change log string

--- printrun/pronsole.py
+++ printrun/pronsole.py
@@ -402,9 +402,9 @@ class pronsole(cmd.Cmd):
             return
         else:
             if self.processing_rc:
-                logging.debug(_("Ignored configuration command") + f" '{l}'")
+                logging.debug(_("Ignored configuration command '{}'" .format(l)))
             else:
-                self.log(_("Unknown command") + f" '{l}'")
+                self.log(_("Unknown command '{}'".format(l)))
 
     def do_exit(self, l):
         if self.p.printing and l != "force":
